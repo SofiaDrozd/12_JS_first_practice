4226d22d485093ecb4ca1bafef8c7106bb6cc086
SofiaDrozd <drozdsofia2005@gmail.com>
2024-10-20 21:59:10 -0900

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,61 @@
+// Завдання на змінні 
+
+// 1)
+const a = 5;
+const b = 2;
+console.log('a * b: ', a * b);
+
+// 2)
+const c = 144;
+const d = 36;
+const result = c / d;
+console.log('c / d: ', result);
+
+// 3)
+const e = 15;
+const f = 55;
+const result1 = e + f;
+console.log('e + f: ', result1);
+
+// 4)
+console.log('typeof 11: ', typeof 11);
+console.log('typeof true: ', typeof true);
+console.log('typeof "java script": ', typeof "java script");
+console.log('typeof "100": ', typeof "100");
+
+// 5)
+let num = 1;
+num += 11;
+num -= 11;
+num *= 11;
+num /= 11;
+num++;
+num--;
+
+// Завдання на введення/виведення 
+
+// 1)
+const num1 = prompt('Введіть число');
+const res = num1 ** 2;
+alert(res);
+
+// 2)
+const num2 = +prompt('Введіть перше число');
+const num3 = +prompt('Введіть друге число');
+const res1 =(num2 + num3) / 2;
+alert('Середнє арифметичне ' + res1);
+
+// 3)
+const num4 = prompt('Введіть кількість хвилин');
+const res2= num4 * 60;
+alert(num4 + ' хвилин у секундах = ' + res2);
+
+// 4)
+const greeting = 'Hello';
+const userName = prompt('What`s your name?');
+alert(greeting + ',' + ' '+ userName);
+
+
+
+
+
